3e1e7162d772dbe2d4c4d82e0d6c5e97fe425dba
Rejman <rejmans00@gmail.com>
2025-10-13 21:00:41 -0300

    feat: dedicated event

--- src/extension.ts
+++ src/extension.ts
@@ -1,49 +1,20 @@
 import "reflect-metadata";
 import * as fs from 'fs';
-import { COMPOSER_FILE, PHP_EXTENSION } from '@infra/utils/constants';
-import { ConfigKeys } from '@domain/workspace/ConfigurationLocator';
+import { FileRenameEvent, workspace } from 'vscode';
+import { COMPOSER_FILE } from '@infra/utils/constants';
 import { container } from "tsyringe";
-import { FeatureFlagManager } from '@domain/workspace/FeatureFlagManager';
-import { ImportRemover } from '@app/services/remove/ImportRemover';
-import { MissingClassImporter } from '@app/services/MissingClassImporter';
-import { NamespaceBatchUpdater } from '@app/services/NamespaceBatchUpdater';
-import { workspace } from 'vscode';
+import { FileRenameHandler } from '@app/events/FileRenameHandler';
 import { WorkspacePathResolver } from './domain/workspace/WorkspacePathResolver';
 
 export function activate() {
-  const files: string[] = fs.readdirSync(new WorkspacePathResolver().getRootPath());
+  const workspacePathResolver = container.resolve(WorkspacePathResolver);
+  const files = fs.readdirSync(workspacePathResolver.getRootPath());
   if (!files.includes(COMPOSER_FILE)) {
     return;
   }
 
-  workspace.onDidRenameFiles(async (event) => {
-    const importRemover = container.resolve(ImportRemover);
-    const missingClassImporter = container.resolve(MissingClassImporter);
-    const namespaceBatchUpdater = container.resolve(NamespaceBatchUpdater);
-    const featureFlagManager = container.resolve(FeatureFlagManager);
-
-    for (const { oldUri, newUri } of event.files) {
-      if (!oldUri.fsPath.endsWith(PHP_EXTENSION)
-        || !newUri.fsPath.endsWith(PHP_EXTENSION)) {
-        continue;
-      }
-
-      try {
-        await namespaceBatchUpdater.execute({ newUri, oldUri });
-
-        if (featureFlagManager.isActive({ key: ConfigKeys.AUTO_IMPORT_NAMESPACE })) {
-          await missingClassImporter.execute({
-            oldUri,
-            newUri,
-          });
-        }
-
-        await importRemover.execute({ uri: newUri });
-      } catch (error) {
-        // eslint-disable-next-line no-undef
-        console.error('Error processing file rename:', error);
-        throw error;
-      }
-    }
+  const handler = container.resolve(FileRenameHandler);
+  workspace.onDidRenameFiles(async (event: FileRenameEvent) => {
+    await handler.handle(event);
   });
 }
